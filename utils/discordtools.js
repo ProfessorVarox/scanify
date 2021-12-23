40aa7cb35f45631199265cf69f19697370fe6e6d
Johannes von der Meden <johannes.meden@infinity-systems.eu>
2021-12-23 15:23:59 +0100

    Removed sensitive details

--- utils/discordtools.js
+++ utils/discordtools.js
@@ -1,7 +1,7 @@
 var moment = require("moment")
 module.exports.notify = function ntfy(msg) {
     const Discord = require("discord.js")
-    let wh = new Discord.WebhookClient("765226444556206120","***REMOVED***")
+    let wh = new Discord.WebhookClient("**REMOVED**","**REMOVED**")
     let embed = new Discord.MessageEmbed()
     embed.setColor("#5BE5B5")
     embed.setTitle("Scanify Log")
